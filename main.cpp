7c35b9c3c8fc61198196bff2f39a8077434e2cc7
Ephraim777 <165553036+Ephraim777@users.noreply.github.com>
2024-05-20 15:13:02 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main()
     MyString c("s");
     MyString f ("s");
     MyString d("privet");
-    MyString f("s");
+    //MyString f("s");
     MyString f1 ("2");
     MyString f2;
     f1 = std::move(c);
